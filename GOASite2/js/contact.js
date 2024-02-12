038969077fc5c4abb8e222d9fc78251d56968aa6
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-02-12 23:01:15 +0400

    Add files via upload

---
+++ GOASite2/js/contact.js
@@ -0,0 +1,12 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  
+  setTimeout(function () {
+    window.location.href = 'index.html';
+  }, 1000); 
+});
+
+
+
+
+
