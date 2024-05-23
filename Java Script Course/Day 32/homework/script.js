c7e0317599333af5a0788e992595548a9f10117d
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-05-23 22:42:13 +0400

    Add files via upload

---
+++ Java Script Course/Day 32/homework/script.js
@@ -0,0 +1,35 @@
+// 1)
+// function sumArrayElements(arr) {
+//     let sum = 0;
+//     arr.forEach(function(num) {
+//         sum += num;
+//     });
+//     return sum;
+// }
+
+
+// 2)
+
+// function logArrayElements(arr) {
+//     arr.forEach(function(element, index) {
+//         console.log("Element at index " + index + ": " + element);
+//     });
+// }
+
+
+// 3)
+
+// function productArrayElements(arr) {
+//     return arr.reduce(function(acc, num) {
+//         return acc * num;
+//     }, 1);
+// }
+
+
+// 4)
+
+// function mergeArrays(arrays) {
+//     return arrays.reduce(function(merged, currentArray) {
+//         return merged.concat(currentArray);
+//     }, []);
+// }
