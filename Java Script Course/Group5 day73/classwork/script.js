3cb45e6a74f788d173aa0957cbfee2dbdb5b5f52
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-06-27 14:03:19 +0400

    Add files via upload

---
+++ Java Script Course/Group5 day73/classwork/script.js
@@ -0,0 +1,20 @@
+const foods = new Array("Khinkali","Khachapuri","Lobiani")
+const foods1 = ["Mwvadi","Sacivi","Gozinaki"]
+
+
+foods[1] = "Shqmeruli"
+foods1[2] = "Soko Kecze"
+foods.push("Xamsa")
+foods1.push("Kubdari")
+
+foods.pop()
+foods.pop()
+foods.pop()
+
+foods1.pop()
+foods1.pop()
+foods1.pop()
+
+console.log(foods)
+console.log(foods1)
+
