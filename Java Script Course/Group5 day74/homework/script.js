63eda6ddd70a5f41c8d5d247f950775086feae05
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-07-16 15:17:42 +0400

    Add files via upload

---
+++ Java Script Course/Group5 day74/homework/script.js
@@ -0,0 +1,125 @@
+// 1)
+
+// const today = new Date();
+
+// const dd = today.getDate();
+
+// const mm = today.getMonth() + 1;
+
+// const yyyy = today.getFullYear();
+
+// today = dd + '/' + mm + '/' + yyyy;
+// console.log(today); 
+
+//2)
+
+// const now = new Date();
+// const hours = now.getHours();
+// const minutes = now.getMinutes()
+
+// today = hours + ":" + minutes
+
+// console.log(today)
+
+// 3)
+
+
+
+
+// MATH
+
+// 2)
+
+// const num = 9
+// const sqrt = Math.sqrt(num)
+// console.log(sqrt)
+
+
+
+// 3)
+
+// const num = 3.12312315123
+
+// const round = Math.round(num)
+// console.log(round)
+
+// 4)
+// const num = 3.12312315123
+
+// const floor = Math.floor(num)
+// console.log(floor)
+
+
+// 5)
+// const num = 3.12312315123
+
+// const ceil = Math.ceil(num)
+// console.log(ceil)
+
+
+//  6)
+// const num1 = 3
+// const num2 = 4
+
+// const pow1 = Math.pow(num1,num2)
+// const pow2 = Math.pow(num2,num1)
+// console.log(pow1)
+// console.log(pow2)
+
+// 7)
+// const num1 = -213
+// const num2 = 1235
+
+// const abs1 = Math.abs(num1)
+// const abs2 = Math.abs(num2)
+// console.log(abs1)
+// console.log(abs2)
+
+
+// 8)
+// const nums = [1,2,3,4,5,6,7,8,9,10]
+// const max = Math.max(...nums)
+
+// console.log(max)
+
+// 9)
+
+// const num = 1
+
+// const random = Math.random(num) > 0.5
+
+// console.log(random)
+
+// 10)
+
+// setInterval
+// function randomNum() {
+//     setInterval(function() {
+//         const randomNumber = Math.floor(Math.random() * 100) + 1;
+//         console.log(randomNumber);
+//     }, 2000);
+// }
+
+// randomNum()
+
+
+
+// setTimeout
+
+// 1)
+// setTimeout(function() {
+//         alert("Hello!");
+//     }, 2000);
+    
+// 2)
+
+//  let count = 5;
+//     let timer = setInterval(function() {
+//         if (count === 0) {
+//             clearInterval(timer);
+//         } else {
+//             console.log(count);
+//             count--;
+//         }
+//     }, 1000);
+
