49935c9cdedc33f4de086540515878721a8bb16a
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-07-24 17:56:45 +0400

    Add files via upload

---
+++ Java Script Course/Group5 day78/homework/script.js
@@ -0,0 +1,31 @@
+// 1)
+
+// let parent = document.getElementById('div');
+// let childNodes = parent.childNodes;
+// console.log(childNodes[0].nodeName);
+// ზუსტად იგიენარიად შეიძლება გაკეთდეს სიებზეც
+
+
+// 2)
+
+// let div = document.getElementById('div');
+// let child = div.firstElementChild;
+// child.style.color = 'red';
+// ზუსტად იგიენარიად შეიძლება გაკეთდეს სიებზეც
+
+
+
+// 3)
+
+
+// let div = document.getElementById('div');
+// let lastChild = div.lastChild;
+// console.log(lastChild);
+
+// ზუსტად იგიენარიად შეიძლება გაკეთდეს სიებზეც
+
+
+// 4) 
+
+let div = document.getElementById('div');
+console.log(div.hasChildNodes());
