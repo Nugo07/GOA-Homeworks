9b5e92b0d0e6dd8f7773f3505c2cf9402b30f3e0
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-08-05 20:03:20 +0400

    Add files via upload

---
+++ Java Script Course/Group5 day82/homework/script.js
@@ -0,0 +1,98 @@
+//1)
+
+// let parent = document.getElementById('div');
+// let children = parent.children
+// console.log(children)
+
+//2)
+
+// let div = document.getElementById('div');
+// let firstElementChild = div.firstElementChild;
+// console.log(firstElementChild)
+
+
+
+
+//3)
+
+
+// let div = document.getElementById('div');
+// let lastElementChild = div.lastElementChild;
+// console.log(lastElementChild);
+
+
+
+
+
+//4) 
+
+// let div = document.getElementById('div1');
+// let nextElementSibling = div.nextElementSibling;
+// console.log(nextElementSibling)
+
+// let div = document.getElementById('div2');
+// let previousElementSibling = div.previousElementSibling;
+// console.log(previousElementSibling)
+
+
+// const div = document.getElementById('div');
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     div.style.backgroundColor = 'red'
+// })
+
+
+// const myp = document.getElementsByClassName('p');
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     myp.style.color = 'blue'
+// })
+
+
+// const myp = document.getElementsByTagName('p');
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     div.style.backgroundColor = 'red'
+// })
+
+
+
+// const div = document.getElementById('div');
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     div.style.backgroundColor = 'red'
+// })
+
+
+// const myps = document.querySelector('.p');
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     div.style.backgroundColor = 'red'
+// })
+
+// const myps = document.querySelectorAll('.p');
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     div.style.backgroundColor = 'red'
+// })
+// const element2 = prompt('Enter Element Name');
+// if(element2){
+//     const newElement = document.createElement(element2)
+//     newElement.textContent = element2
+//     document.body.appendChild(newElement)
+// }else{
+//     alert('error')
+// }
+
+
+
+
+
+
+
