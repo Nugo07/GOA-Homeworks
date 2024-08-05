413f47b029fabf757b6322632a5e4bf8dcfcfba7
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-08-05 19:07:42 +0400

    Add files via upload

---
+++ Java Script Course/Group5 day80/classwork/script.js
@@ -0,0 +1,32 @@
+// const p  = document.getElementById('myp')
+
+// setInterval(function(){
+// p.textContent = "Hello User"
+// p.style.color = "white"
+// p.style.backgroundColor = "black"
+// p.style.textAlign = "center"
+// },3000)
+
+
+// DOM = Document Object Model, romelic aris HTML obieqtis saxit, xolo es obieqti JS-shi warmodgenilia document saxit, DOM is daxmarebit chven vaxdent manipulacias HTML da shegvidlia misi nebismieri elementis romelic body tagshia moqceuli shevuccvalot feri zoma da aseshemdeg...
+
+
+// getElementById abrunebs elements id-is shesabamisad
+const p1 = document.getElementById('myp')
+console.log(p1)    
+
+// getElementsByClassName
+const p2 = document.getElementsByClassName('myps')
+console.log(p2)
+
+
+// querySelector moaqvs is elementi romelic pirveli shexvdeba
+const p3 = document.querySelector('p')
+console.log(p3)
+
+
+// querySelecctorAll moaqvs yvelaferi saxelis shesabamisad
+const p4 = document.querySelectorAll('p')
+
+// getElementsByTagName moaqvs elementi tagis saxelis mixedvit
+const p5 = document.getElementsByTagName('p')
