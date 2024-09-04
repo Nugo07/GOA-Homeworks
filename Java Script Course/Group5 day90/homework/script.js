dffd072b085b3560f924707b901d9940744703db
Nugo07 <142174578+Nugo07@users.noreply.github.com>
2024-09-04 22:52:21 +0400

    Add files via upload

---
+++ Java Script Course/Group5 day90/homework/script.js
@@ -0,0 +1,39 @@
+// // 2. 
+// const person = {
+//     name: 'Nugzara',
+//     age: 17,
+// };
+
+// // 3. 
+// let newPerson = Object.assign({}, person, {
+//     phone: '595030279',
+//     email: 'zangizangi20@gamil.com'
+// });
+
+// // 4. 
+// const myHouse = {
+//     surname: 'Turmanishvili',
+//     address: 'Tbilis Street 22',
+// };
+
+// const updatedPerson = Object.assign({}, person, myHouse);
+
+// // 5. 
+// const { name, age,} = person;
+
+
+// console.log('Name:', name); 
+// console.log('Age:', age);
+ 
+
+
+// // 6. 
+// const array = ['apple', 'banana', 'cherry'];
+
+
+// const [first, second, third] = array;
+
+
+// console.log('First:', first); 
+// console.log('Second:', second); 
+// console.log('Third:', third); 
